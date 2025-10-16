81b0847a0a6389580384764c7be3ae3b7bdf2672
speeddragon <speeddragon@gmail.com>
2025-10-16 15:52:11 +0100

    Improve code error handling, tests and test documentation

--- src/hb_store.erl
+++ src/hb_store.erl
@@ -439,19 +439,8 @@ test_stores() ->
                     <<"name">> => <<"cache-TEST/lru">>
                 }
             ]
-        },
-        %% Should S3 be under a feature flag? 
-        (hb_test_utils:test_store(hb_store_s3))#{
-            %% NOTE: To be tuned
-            <<"benchmark-scale">> => 0.005,
-            %% Default config
-            <<"bucket">> => <<"hb-s3">>,
-            <<"access-key-id">> => <<"niko">>,
-            <<"secret-access-key">> => <<"minio-niko-rocks">>,
-            <<"endpoint">> => <<"localhost:9000">>,
-            <<"dangerous_reset">> => true
         }
-    ] ++ rocks_stores().
+    ] ++ rocks_stores() ++ s3_stores().
 
 -ifdef(ENABLE_ROCKSDB).
 rocks_stores() ->
@@ -464,7 +453,15 @@ rocks_stores() ->
 -else.
 rocks_stores() -> [].
 -endif.
-
+-ifdef(ENABLE_S3).
+s3_stores() ->
+    [(hb_store_s3:default_test_opts())#{
+        %% NOTE: To be tuned
+        <<"benchmark-scale">> => 0.005
+    }].
+-else.
+s3_stores() -> [].
+-endif.
 generate_test_suite(Suite) ->
     generate_test_suite(Suite, test_stores()).
 generate_test_suite(Suite, Stores) ->
