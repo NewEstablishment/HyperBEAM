bc765e3d9207b46f5cba35917296c65d3aacfc65
speeddragon <speeddragon@gmail.com>
2026-01-13 13:02:45 +0000

    fix: Handling 500 requests in parallel

--- src/hb_http_multi.erl
+++ src/hb_http_multi.erl
@@ -47,6 +47,7 @@ request(Config, Method, Path, Message, Opts) ->
         admissible_status := Statuses,
         parallel := Parallel
     } = multirequest_opts(Config, Message, Opts),
+    erlang:display({parallel, Parallel}),
     MultirequestMsg =
         hb_message:without_unless_signed(
             lists:filter(
@@ -232,12 +233,20 @@ admissible_response(Response, Msg, Opts) ->
     ?event(debug_multi,
         {executing_admissible_message, {message, Base}, {req, Req}}
     ),
-    case hb_ao:resolve(Base, Req, Opts) of
+    try hb_ao:resolve(Base, Req, Opts) of
         {ok, Res} when is_atom(Res) or is_binary(Res) ->
             ?event(debug_multi, {admissible_result, {result, Res}}),
             hb_util:atom(Res) == true;
         {error, Reason} ->
             ?event(debug_multi, {admissible_error, {reason, Reason}}),
+            false
+    catch 
+        Class:Reason:Stacktrace ->
+            ?event(error, 
+                {admissible_response, 
+                    {class, Class}, 
+                    {reason, Reason}, 
+                    {stacktrace, Stacktrace}}),
             false
     end.
 
