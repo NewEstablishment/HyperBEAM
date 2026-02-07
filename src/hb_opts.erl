3322ad7d2c52a631dfd61afa5c9c203c66e693b3
speeddragon <speeddragon@gmail.com>
2026-02-07 15:08:29 +0000

    impr: Do not show store_error and track unsupported_tx

--- src/hb_opts.erl
+++ src/hb_opts.erl
@@ -25,7 +25,7 @@
 -else.
 -define(DEFAULT_PRINT_OPTS,
     [
-        error, http_error, cron_error, store_error,
+        error, http_error, cron_error,
         http_short, compute_short, push_short, copycat_short
     ]
 ).
