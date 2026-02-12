b101534121c59a973e2d52a3800c821e53e5b9e8
speeddragon <speeddragon@gmail.com>
2026-02-12 01:14:36 +0000

    fix: hb_store_arweave metrics

--- src/hb_store_arweave.erl
+++ src/hb_store_arweave.erl
@@ -44,13 +44,13 @@ read(StoreOpts = #{ <<"index-store">> := IndexStore }, ID) ->
             end,
             case Loaded of
                 {ok, _Message} ->
-                    ?event({{read, ok},
+                    ?event(arweave_store, {chunks_found,
                         {id, {explicit, ID}},
                         {is_tx, IsTX},
                         {start_offset, StartOffset},
                         {length, Length}});
                 {error, Reason} ->
-                    ?event({{read, error}, 
+                    ?event(arweave_store, {chunks_not_found,
                         {id, {explicit, ID}}, 
                         {is_tx, IsTX},
                         {start_offset, StartOffset},
@@ -59,39 +59,8 @@ read(StoreOpts = #{ <<"index-store">> := IndexStore }, ID) ->
             end,
             Loaded;
         not_found ->
-            case hb_store:read(IndexStore, path(ID)) of
-                {ok, Binary} ->
-                    [IsTX, StartOffset, Length] = binary:split(Binary, <<":">>, [global]),
-                    Result = case hb_util:bool(IsTX) of
-                        true ->
-                            load_bundle(ID,
-                                hb_util:int(StartOffset), hb_util:int(Length), StoreOpts);
-                        false ->
-                            load_item(
-                                hb_util:int(StartOffset), hb_util:int(Length), StoreOpts)
-                    end,
-                    case Result of 
-                        {ok, Message} ->
-                            ?event(arweave_store, {chunks_found, {id, ID}, {message, Message}}),
-                            %% Cannot be async, because it will conflict with some binary 
-                            %% transactions.
-                            %% This transactions makes fallback to hyperbuddy logic, which give 
-                            %% 500 on first try, following by 200.
-                            hb_store_remote_node:maybe_cache(StoreOpts, Message),
-                            {ok, Message};
-                        {error, Reason} ->
-                            ?event(arweave_store, {chunks_not_found, {id, ID}}),
-                            ?event(error, {hb_store_arweave_local_store, {reason, Reason}}),
-                            {error, Reason}
-                    end;
-                not_found ->
-                    ?event(arweave_store, {no_index_found, {id, ID}}),
-                    {error, not_found}
-            end;
-
-        {ok, Data} ->
-            ?event(arweave_store, {local_cache_found, {id, ID}}),
-            {ok, Data}
+            ?event(arweave_store, {no_index_found, {id, ID}}),
+            {error, not_found}
     end;
 read(_, _) -> 
     {error, not_found}.
